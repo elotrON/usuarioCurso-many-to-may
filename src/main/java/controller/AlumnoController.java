b44a339e05fb2d6034b9208752c35beb58873a53
eloy <eloy.elnet@gmail.com>
2026-03-24 12:12:05 +0100

    agregado endpoint para crear alumno

--- src/main/java/controller/AlumnoController.java
+++ src/main/java/controller/AlumnoController.java
@@ -1,9 +1,25 @@
 package controller;
 
+import dto.AlumnoRequest;
+import dto.AlumnoResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.AlumnoService;
 
 @RestController
 public class AlumnoController {
+
+    AlumnoService alumnoService;
+
+    public AlumnoController(AlumnoService alumnoService) {
+        this.alumnoService = alumnoService;
+    }
+
+    @PostMapping("/alumno")
+    public AlumnoResponse nuevoAlumno(AlumnoRequest alumnoRequest){
+        return alumnoService.crearAlumno(alumnoRequest);
+    }
 
 
 }
